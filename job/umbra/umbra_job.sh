e5d82d5b8d1ce03139d6ef3a8bc48d191f957099
Alice Rey <alice.rey@tum.de>
2022-06-01 14:04:27 +0200

    Update umbra_job.sh

--- job/umbra/umbra_job.sh
+++ job/umbra/umbra_job.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
-echo $SCRIPT_DIR
-
 docker run -it --privileged -v ${PWD}:/umbra -v $SCRIPT_DIR:/scripts --entrypoint /scripts/entrypoint.sh ghcr.io/alicerey/umbra-release:30908e0cd
